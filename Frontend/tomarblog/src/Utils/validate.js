93830ce733d1250ef41a3d26fabda407d5fe35f7
yabets143 <139507335+yabets143@users.noreply.github.com>
2024-10-21 11:36:37 +0300

    create a validation util for password and email fields

---
+++ Frontend/tomarblog/src/Utils/validate.js
@@ -0,0 +1,10 @@
+// /src/utils/validate.js
+export const validateEmail = (email) => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  };
+  
+  export const validatePassword = (password) => {
+    return password.length >= 6; // Example validation
+  };
+  
